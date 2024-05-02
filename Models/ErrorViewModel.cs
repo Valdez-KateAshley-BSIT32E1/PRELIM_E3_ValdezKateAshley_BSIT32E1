d8dc9da7190eeb9bb3b2958937812739a1b11da7
kateashely <valdezkateashley19@gmail.com>
2024-05-02 12:42:21 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_ValdezKateAshley_BSIT32E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
